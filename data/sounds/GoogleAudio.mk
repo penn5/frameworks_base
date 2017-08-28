401a6a8cfbcdd259819a1c012a6b8737c4a8720d
ezio84 <brabus84@gmail.com>
2017-08-28 10:12:48 +0200

    Switch to Pixel 2XL 8.1 sounds [2/2]
    Change-Id: Ic571cd228dbfcad1992d0071cc4b649f845fdc05
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- data/sounds/GoogleAudio.mk
+++ data/sounds/GoogleAudio.mk
@@ -15,7 +15,6 @@
 LOCAL_PATH := frameworks/base/data/sounds/
 
 PRODUCT_COPY_FILES += \
-<<<<<<< HEAD
     $(LOCAL_PATH)material/alarms/A_real_hoot.ogg:system/media/audio/alarms/A_real_hoot.ogg \
     $(LOCAL_PATH)material/alarms/Bright_morning.ogg:system/media/audio/alarms/Bright_morning.ogg \
     $(LOCAL_PATH)material/alarms/Cuckoo_clock.ogg:system/media/audio/alarms/Cuckoo_clock.ogg \
@@ -28,39 +27,12 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)material/alarms/Rolling_fog.ogg:system/media/audio/alarms/Rolling_fog.ogg \
     $(LOCAL_PATH)material/alarms/Spokes.ogg:system/media/audio/alarms/Spokes.ogg \
     $(LOCAL_PATH)material/alarms/Sunshower.ogg:system/media/audio/alarms/Sunshower.ogg \
-=======
-    $(LOCAL_PATH)material/alarms/Argon.ogg:system/media/audio/alarms/Argon.ogg \
-    $(LOCAL_PATH)material/alarms/Awaken.ogg:system/media/audio/alarms/Awaken.ogg \
-    $(LOCAL_PATH)material/alarms/Bounce.ogg:system/media/audio/alarms/Bounce.ogg \
-    $(LOCAL_PATH)material/alarms/Carbon.ogg:system/media/audio/alarms/Carbon.ogg \
-    $(LOCAL_PATH)material/alarms/Drip.ogg:system/media/audio/alarms/Drip.ogg \
-    $(LOCAL_PATH)material/alarms/Gallop.ogg:system/media/audio/alarms/Gallop.ogg \
-    $(LOCAL_PATH)material/alarms/Helium.ogg:system/media/audio/alarms/Helium.ogg \
-    $(LOCAL_PATH)material/alarms/Krypton.ogg:system/media/audio/alarms/Krypton.ogg \
-    $(LOCAL_PATH)material/alarms/Neon.ogg:system/media/audio/alarms/Neon.ogg \
-    $(LOCAL_PATH)material/alarms/Nudge.ogg:system/media/audio/alarms/Nudge.ogg \
-    $(LOCAL_PATH)material/alarms/Orbit.ogg:system/media/audio/alarms/Orbit.ogg \
-    $(LOCAL_PATH)material/alarms/Osmium.ogg:system/media/audio/alarms/Osmium.ogg \
-    $(LOCAL_PATH)material/alarms/Oxygen.ogg:system/media/audio/alarms/Oxygen.ogg \
-    $(LOCAL_PATH)material/alarms/Platinum.ogg:system/media/audio/alarms/Platinum.ogg \
-    $(LOCAL_PATH)material/alarms/Rise.ogg:system/media/audio/alarms/Rise.ogg \
-    $(LOCAL_PATH)material/alarms/Sway.ogg:system/media/audio/alarms/Sway.ogg \
-    $(LOCAL_PATH)material/alarms/Timer.ogg:system/media/audio/alarms/Timer.ogg \
-    $(LOCAL_PATH)material/alarms/Wag.ogg:system/media/audio/alarms/Wag.ogg \
->>>>>>> 5b5afb7... Build custom audio files [2/2]
     $(LOCAL_PATH)material/effects/audio_end.ogg:system/media/audio/ui/audio_end.ogg \
     $(LOCAL_PATH)material/effects/audio_initiate.ogg:system/media/audio/ui/audio_initiate.ogg \
     $(LOCAL_PATH)material/effects/camera_click.ogg:system/media/audio/ui/camera_click.ogg \
     $(LOCAL_PATH)material/effects/camera_focus.ogg:system/media/audio/ui/camera_focus.ogg \
-<<<<<<< HEAD
-    $(LOCAL_PATH)material/effects/ChargingStarted.ogg:system/media/audio/ui/ChargingStarted.ogg \
     $(LOCAL_PATH)material/effects/Dock.ogg:system/media/audio/ui/Dock.ogg \
     $(LOCAL_PATH)material/effects/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
-    $(LOCAL_PATH)material/effects/InCallNotification.ogg:system/media/audio/ui/InCallNotification.ogg \
-=======
-    $(LOCAL_PATH)material/effects/Dock.ogg:system/media/audio/ui/Dock.ogg \
-    $(LOCAL_PATH)material/effects/Effect_Tick.ogg:system/media/audio/ui/Effect_Tick.ogg \
->>>>>>> 5b5afb7... Build custom audio files [2/2]
     $(LOCAL_PATH)material/effects/KeypressDelete.ogg:system/media/audio/ui/KeypressDelete.ogg \
     $(LOCAL_PATH)material/effects/KeypressInvalid.ogg:system/media/audio/ui/KeypressInvalid.ogg \
     $(LOCAL_PATH)material/effects/KeypressReturn.ogg:system/media/audio/ui/KeypressReturn.ogg \
@@ -79,7 +51,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)material/effects/VideoRecord.ogg:system/media/audio/ui/VideoRecord.ogg \
     $(LOCAL_PATH)material/effects/VideoStop.ogg:system/media/audio/ui/VideoStop.ogg \
     $(LOCAL_PATH)material/effects/WirelessChargingStarted.ogg:system/media/audio/ui/WirelessChargingStarted.ogg \
-<<<<<<< HEAD
     $(LOCAL_PATH)material/notifications/Beginning.ogg:system/media/audio/notifications/Beginning.ogg \
     $(LOCAL_PATH)material/notifications/Coconuts.ogg:system/media/audio/notifications/Coconuts.ogg \
     $(LOCAL_PATH)material/notifications/Duet.ogg:system/media/audio/notifications/Duet.ogg \
@@ -107,28 +78,3 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)material/ringtones/Schools_out.ogg:system/media/audio/ringtones/Schools_out.ogg \
     $(LOCAL_PATH)material/ringtones/The_big_adventure.ogg:system/media/audio/ringtones/The_big_adventure.ogg \
     $(LOCAL_PATH)material/ringtones/Zen_too.ogg:system/media/audio/ringtones/Zen_too.ogg
-=======
-    $(LOCAL_PATH)material/notifications/Ariel.ogg:system/media/audio/notifications/Ariel.ogg \
-    $(LOCAL_PATH)material/notifications/Carme.ogg:system/media/audio/notifications/Carme.ogg \
-    $(LOCAL_PATH)material/notifications/Ceres.ogg:system/media/audio/notifications/Ceres.ogg \
-    $(LOCAL_PATH)material/notifications/Elara.ogg:system/media/audio/notifications/Elara.ogg \
-    $(LOCAL_PATH)material/notifications/Europa.ogg:system/media/audio/notifications/Europa.ogg \
-    $(LOCAL_PATH)material/notifications/Iapetus.ogg:system/media/audio/notifications/Iapetus.ogg \
-    $(LOCAL_PATH)material/notifications/Io.ogg:system/media/audio/notifications/Io.ogg \
-    $(LOCAL_PATH)material/notifications/Rhea.ogg:system/media/audio/notifications/Rhea.ogg \
-    $(LOCAL_PATH)material/notifications/Salacia.ogg:system/media/audio/notifications/Salacia.ogg \
-    $(LOCAL_PATH)material/notifications/Tethys.ogg:system/media/audio/notifications/Tethys.ogg \
-    $(LOCAL_PATH)material/notifications/Titan.ogg:system/media/audio/notifications/Titan.ogg \
-    $(LOCAL_PATH)material/ringtones/Atria.ogg:system/media/audio/ringtones/Atria.ogg \
-    $(LOCAL_PATH)material/ringtones/Callisto.ogg:system/media/audio/ringtones/Callisto.ogg \
-    $(LOCAL_PATH)material/ringtones/Dione.ogg:system/media/audio/ringtones/Dione.ogg \
-    $(LOCAL_PATH)material/ringtones/Ganymede.ogg:system/media/audio/ringtones/Ganymede.ogg \
-    $(LOCAL_PATH)material/ringtones/Luna.ogg:system/media/audio/ringtones/Luna.ogg \
-    $(LOCAL_PATH)material/ringtones/Oberon.ogg:system/media/audio/ringtones/Oberon.ogg \
-    $(LOCAL_PATH)material/ringtones/Phobos.ogg:system/media/audio/ringtones/Phobos.ogg \
-    $(LOCAL_PATH)material/ringtones/Pyxis.ogg:system/media/audio/ringtones/Pyxis.ogg \
-    $(LOCAL_PATH)material/ringtones/Sedna.ogg:system/media/audio/ringtones/Sedna.ogg \
-    $(LOCAL_PATH)material/ringtones/Titania.ogg:system/media/audio/ringtones/Titania.ogg \
-    $(LOCAL_PATH)material/ringtones/Triton.ogg:system/media/audio/ringtones/Triton.ogg \
-    $(LOCAL_PATH)material/ringtones/Umbriel.ogg:system/media/audio/ringtones/Umbriel.ogg \
->>>>>>> 5b5afb7... Build custom audio files [2/2]
