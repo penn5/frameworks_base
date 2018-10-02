dca2d5e5cd3cbbc17324b51978122082b1313292
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-10-02 01:51:02 +0000

    microG fixup

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -4017,7 +4017,7 @@ public class PackageManagerService extends IPackageManager.Stub
                     && p.mAppMetaData != null) {
                 String sig = p.mAppMetaData.getString("fake-signature");
                 if (sig != null) {
-                    pi.signatures = new Signature {new Signature(sig)};
+                    pi.signatures = new Signature[] {new Signature(sig)};
                 }
             }
         } catch (Throwable t) {
