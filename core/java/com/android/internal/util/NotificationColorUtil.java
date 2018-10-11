8b28ed7c6b744d0f262b229182cf19de68399784
Dil3mm4 <dil3mm4.dev@gmail.com>
2018-10-11 17:34:15 +0000

    Missing stuff for dark notif

--- core/java/com/android/internal/util/NotificationColorUtil.java
+++ core/java/com/android/internal/util/NotificationColorUtil.java
@@ -40,6 +40,7 @@ import android.text.style.TextAppearanceSpan;
 import android.util.Log;
 import android.util.Pair;
 
+import com.android.internal.R;
 import java.util.Arrays;
 import java.util.WeakHashMap;
 
