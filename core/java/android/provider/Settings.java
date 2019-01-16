7946f78ed15896e3527d49014f1277f62d61dc07
Dil3mm4 <dil3mm4.dev@gmail.com>
2019-01-16 18:04:27 +0000

    Revert "BatteryMeterDrawable: Add text style"
    This reverts commit b4011c94d299b53dbb3bcdbf87ae28cabcd1387e.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8061,7 +8061,6 @@ public final class Settings {
          * 0: Display the battery an icon in portrait mode
          * 2: Display the battery as a circle
          * 3: Display the battery as a dotted circle
-         * 5: Display the battery as text
          * default: 0
          * @hide
          */
